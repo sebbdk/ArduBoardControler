151c56dbff128b862f9f3675b5994a93fe3c7922
Kasper Sebb' brandt <sebb@sebb.dk>
2018-07-16 23:28:29 +0200

    Clean up library code

--- ArduBoardControler_Tx/lib/LiPoCheck.cpp
+++ ArduBoardControler_Tx/lib/LiPoCheck.cpp
@@ -1,42 +1,20 @@
-/*
-Copyright 2015 - 2017 Andreas Chaitidis Andreas.Chaitidis@gmail.com
-
-This program is free software : you can redistribute it and / or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
 #include "LiPoCheck.h"
 
 int CountCells(float voltage, int celltype) {
-	
-	for (int i = 1; i <= MAXNUMBERCELLS; i++)
-	{
-		if (celltype == CELL_LIPO)
-		{
-			if ((voltage > ((float)i*LIPO_MINVOLRANGE)) && (voltage < ((float)i*LIPO_MAXVOLRANGE)))
-			{
+	for (int i = 1; i <= MAXNUMBERCELLS; i++) {
+		if (celltype == CELL_LIPO) {
+			if ((voltage > ((float)i*LIPO_MINVOLRANGE)) && (voltage < ((float)i*LIPO_MAXVOLRANGE))) {
 				return i;
 			}
 		}
-		if (celltype == CELL_LIION)
-		{
-			if ((voltage > ((float)i*LIION_MINVOLRANGE)) && (voltage < ((float)i*LIION_MAXVOLRANGE)))
-			{
+
+		if (celltype == CELL_LIION) {
+			if ((voltage > ((float)i*LIION_MINVOLRANGE)) && (voltage < ((float)i*LIION_MAXVOLRANGE))) {
 				return i;
 			}
 		}
 	}
+
 	return 0;
 }
 
@@ -44,73 +22,48 @@ int CapCheckPerc(float voltage, int cells, int celltype) {
 	float voltageCell = 0;
 	int ind = 0;
 
-	switch (celltype)
-	{
-	case 0:
-		if (cells > 0)
-		{
-			voltageCell = (voltage / cells);
-		}
+	switch (celltype) {
+		case 0:
+			if (cells > 0) {
+				voltageCell = (voltage / cells);
+			}
 
-		if (voltageCell >= LIPO_MAXVOLRANGE)
-		{
-			return (100);
-		}
+			if (voltageCell >= LIPO_MAXVOLRANGE) {
+				return (100);
+			}
 
-		//while (!(voltageCell <= lipoDc[0][ind + 1] && voltageCell > lipoDc[0][ind]) && ind <= (sizeof(lipoDc)/sizeof(*lipoDc)))
-		while (!(voltageCell <= lipoDc[0][ind + 1] && voltageCell > lipoDc[0][ind]) && ind <= 10)
-		{
-			ind++;
-		}
+			while (!(voltageCell <= lipoDc[0][ind + 1] && voltageCell > lipoDc[0][ind]) && ind <= 10) {
+				ind++;
+			}
 
-		if (voltageCell <= lipoDc[0][ind + 1] && voltageCell > lipoDc[0][ind])
-		{
-			float CapacPers = (((lipoDc[1][ind + 1] - lipoDc[1][ind]) / (lipoDc[0][ind + 1] - lipoDc[0][ind]))*(voltageCell - lipoDc[0][ind])) + lipoDc[1][ind];
-			//	int intCapacPers = (int)(CapacPers * 100);
-			return (CapacPers * 100);
-		}
-		else
-		{
-			return 0;
+			if (voltageCell <= lipoDc[0][ind + 1] && voltageCell > lipoDc[0][ind]) {
+				float CapacPers = (((lipoDc[1][ind + 1] - lipoDc[1][ind]) / (lipoDc[0][ind + 1] - lipoDc[0][ind]))*(voltageCell - lipoDc[0][ind])) + lipoDc[1][ind];
+				return (CapacPers * 100);
+			}
+			break;
+		case 1:
+			if (cells > 0) {
+				voltageCell = (voltage / cells);
+			}
 
-		}
+			if (voltageCell >= LIION_MAXVOLRANGE) {
+				return (100);
+			}
 
-		break;
-	case 1:
-		if (cells > 0)
-		{
-			voltageCell = (voltage / cells);
-		}
+			while(!(voltageCell <= liionDc[0][ind + 1] && voltageCell > liionDc[0][ind]) && ind <= 6) {
+				ind++;
+			}
 
-		if (voltageCell >= LIION_MAXVOLRANGE)
-		{
-			return (100);
-		}
-
-		//while (!(voltageCell <= liionDc[0][ind + 1] && voltageCell > liionDc[0][ind]) && ind <= (sizeof(liionDc) / sizeof(*liionDc)))
-		while(!(voltageCell <= liionDc[0][ind + 1] && voltageCell > liionDc[0][ind]) && ind <= 6)
-		{
-			ind++;
-		}
-
-		if (voltageCell <= liionDc[0][ind + 1] && voltageCell > liionDc[0][ind])
-		{
-			float CapacPers = (((liionDc[1][ind + 1] - liionDc[1][ind]) / (liionDc[0][ind + 1] - liionDc[0][ind]))*(voltageCell - liionDc[0][ind])) + liionDc[1][ind];
-			//	int intCapacPers = (int)(CapacPers * 100);
-			return (CapacPers * 100);
-		}
-		else
-		{
-			return 0;
-
-		}
-
-		break;
-
-	default:
-		return 0;
-		break;
+			if (voltageCell <= liionDc[0][ind + 1] && voltageCell > liionDc[0][ind]) {
+				float CapacPers = (((liionDc[1][ind + 1] - liionDc[1][ind]) / (liionDc[0][ind + 1] - liionDc[0][ind]))*(voltageCell - liionDc[0][ind])) + liionDc[1][ind];
+				return (CapacPers * 100);
+			}
+			break;
+		default:
+			break;
 	}
+
+	return 0;
 }
 
 
--- ArduBoardControler_Tx/lib/LiPoCheck.h
+++ ArduBoardControler_Tx/lib/LiPoCheck.h
@@ -1,52 +1,27 @@
-// VoltageCheck.h
-
-/*
-Copyright 2015 - 2017 Andreas Chaitidis Andreas.Chaitidis@gmail.com
-
-This program is free software : you can redistribute it and / or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
 #ifndef _LIPOCHECK_h
-#define _LIPOCHECK_h
+	#define _LIPOCHECK_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
-#else
-	#include "WProgram.h"
-#endif
+	#if defined(ARDUINO) && ARDUINO >= 100
+		#include "arduino.h"
+	#else
+		#include "WProgram.h"
+	#endif
 
-#ifndef _CONFIG_h
+	#ifndef _CONFIG_h
+		#include "Config.h"
+	#endif
 
-#include "Config.h"
+	#define CELL_LIPO	0
+	#define LIPO_MINVOLRANGE	3.3
+	#define	LIPO_MAXVOLRANGE	4.2
+	#define CELL_LIION	1
+	#define LIION_MINVOLRANGE	2.5
+	#define	LIION_MAXVOLRANGE	4.2
 
-#endif // !_CONFIG_h
+	const float lipoDc[2][11] = { { 3.2, 3.680,3.740,3.780,3.810,3.850,3.870,3.960,4.02,4.100,4.200 } ,{ 0.000, 0.100,0.200,0.300,0.400,0.500,0.600,0.700,0.800,0.900,1.000 } };
+	const float liionDc[2][7] = { {2.5, 3.4, 3.6, 3.68, 3.75, 3.88, 4.2} ,{ 0.000, 0.100, 0.200,0.400,0.600,0.800, 1.000} }; //source: http://www.ibt-power.com/bkencel/Graphs/LiIonDischGph.JPG
 
-#define CELL_LIPO	0
-#define LIPO_MINVOLRANGE	3.3
-#define	LIPO_MAXVOLRANGE	4.2
-#define CELL_LIION	1
-#define LIION_MINVOLRANGE	2.5
-#define	LIION_MAXVOLRANGE	4.2
-
-//const float lipoDc[2][8] = { { 0.0, 3.670,3.740,3.790,3.820,3.900,4.030,4.200 } , {0.000, 0.100,0.200,0.300,0.400,0.600,0.800,1.000 } };
-const float lipoDc[2][11] = { { 3.2, 3.680,3.740,3.780,3.810,3.850,3.870,3.960,4.02,4.100,4.200 } ,{ 0.000, 0.100,0.200,0.300,0.400,0.500,0.600,0.700,0.800,0.900,1.000 } };
-const float liionDc[2][7] = { {2.5, 3.4, 3.6, 3.68, 3.75, 3.88, 4.2} ,{ 0.000, 0.100, 0.200,0.400,0.600,0.800, 1.000} }; //source: http://www.ibt-power.com/bkencel/Graphs/LiIonDischGph.JPG
-int CountCells(float voltage, int celltype);
-
-int CapCheckPerc(float voltage, int cells, int celltype);
-
-
+	int CountCells(float voltage, int celltype);
+	int CapCheckPerc(float voltage, int cells, int celltype);
 #endif
 
